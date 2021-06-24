3b9b8a59c9bfeb80efcf2a0eeae433e0c35b54d1
Anderson Lasak <anderson.lasak@petlove.com.br>
2021-06-24 17:06:53 -0300

    Script configuration

--- step.sh
+++ step.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 set -ex
 
-echo "This is the value specified for the input 'example_step_input': ${example_step_input}"
+IFS='/' read -ra file_path_split <<< "$raw_file_path"
+file_name=${file_path_split[-1]}
 
-#
-# --- Export Environment Variables for other Steps:
-# You can export Environment Variables for other Steps with
-#  envman, which is automatically installed by `bitrise setup`.
-# A very simple example:
-envman add --key EXAMPLE_STEP_OUTPUT --value 'the value you want to share'
-# Envman can handle piped inputs, which is useful if the text you want to
-# share is complex and you don't want to deal with proper bash escaping:
-#  cat file_with_complex_input | envman add --KEY EXAMPLE_STEP_OUTPUT
-# You can find more usage examples on envman's GitHub page
-#  at: https://github.com/bitrise-io/envman
+echo "File name: ${file_name}"
+echo "Run script: ${run_script}"
 
-#
-# --- Exit codes:
-# The exit code of your Step is very important. If you return
-#  with a 0 exit code `bitrise` will register your Step as "successful".
-# Any non zero exit code will be registered as "failed" by `bitrise`.
+curl -H "Authorization: token $github_access_token" \
+  -H "Accept: application/vnd.github.v3.raw" \
+  -O \
+  -L "$raw_file_path"
+
+echo "Download finished!"
+
+if [ "$run_script" = "yes" ]; then
+    echo "Running script..."
+    bash ./$file_name
+fi
+
+script_result=$?
+exit ${script_result}
